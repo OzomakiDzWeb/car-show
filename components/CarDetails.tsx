b9ed8727e1722c7328ef3804a73a8bdb5a458cdb
OzomakiDzWeb <ayoubbouslama1994@gmail.com>
2024-01-30 06:40:36 +0000

    creatg model

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import {CarsProps} from '../types/index'
+"use client";
+import React from "react";
+import { CarsProps } from "../types/index";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 interface CarDetailsProps {
   isopen: boolean;
   closModel: () => void;
   car: CarsProps;
 }
-const CarDetails = ({ isopen, closModel, car }: CarDetailsProps ) => {
-  return <div>CarDetails</div>;
+const CarDetails = ({ isopen, closModel, car }: CarDetailsProps) => {
+  return (
+    <>
+      <Transition appear show={isopen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closModel}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25"></div>
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel>
+                  <button
+                  type="button"
+                  onClick={closModel}>
+                    <Image sr/>
+                  </button>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 };
 
-export default CarDetails
+export default CarDetails;
